3fdc811ec0ca070fd4999d69d1a4a387090b43fa
Isaac Villanueva Ferrera <166497344+IsaacBoku@users.noreply.github.com>
2025-04-12 12:47:21 +0200

    [Project_roxel] Animator IndicatorInteractive

--- Assets/Scripts/Props/InteractableIndicator.cs
+++ Assets/Scripts/Props/InteractableIndicator.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class InteractableIndicator : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer indicatorSprite;
+    [SerializeField] private Animator indicatorAnimator;
     [SerializeField] private float blinkSpeed = 2f;
     private float blinkTimer = 0f;
 
     void Update()
     {
-        // Hacer que el indicador parpadee para que sea más visible
         blinkTimer += Time.deltaTime * blinkSpeed;
         float alpha = Mathf.PingPong(blinkTimer, 1f);
         Color spriteColor = indicatorSprite.color;
@@ -19,10 +19,12 @@ public class InteractableIndicator : MonoBehaviour
     public void Show()
     {
         indicatorSprite.enabled = true;
+        indicatorAnimator.SetBool("IsVisible", true);
     }
 
     public void Hide()
     {
         indicatorSprite.enabled = false;
+        indicatorAnimator.SetBool("IsVisible", false);
     }
 }
