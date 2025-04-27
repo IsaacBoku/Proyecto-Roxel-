402e4f4f376cb17d41de766d393c2cfe5263c63e
Isaac Villanueva Ferrera <166497344+IsaacBoku@users.noreply.github.com>
2025-04-27 16:53:15 +0200

    [Project_Roxel] Cambio prefab pedestal ajustes

--- Assets/SceneSwitcherPro/Editor/SceneSwitcherToolbar.cs
+++ Assets/SceneSwitcherPro/Editor/SceneSwitcherToolbar.cs
@@ -172,7 +172,7 @@ public static class SceneSwitcherToolbar
     static void LoadScene(string sceneName)
     {
         string scenePath;
-        Debug.Log("<b><color=green>Thank you for using the package  -Ajay</color></b>");
+        //Debug.Log("<b><color=green>Thank you for using the package  -Ajay</color></b>");
 
         if (fetchAllScenes)
         {
