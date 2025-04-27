4cf4eb5137b3897d6af4a0143efba798649a9d40
Isaac Villanueva Ferrera <166497344+IsaacBoku@users.noreply.github.com>
2025-04-27 19:55:10 +0200

    [Project_Roxel] Mejora button mechanic

--- Assets/Scripts/Props/Objects/Switch_Mechanic.cs
+++ Assets/Scripts/Props/Objects/Switch_Mechanic.cs
@@ -3,81 +3,183 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Switch_Mechanic : MonoBehaviour
+public class Switch_Mechanic : InteractableBase
 {
-    [Header("Door")]
-    [SerializeField] private Door_Mechanic[] doorOpen;
-    [SerializeField] private Door_Mechanic[] doorClosed;
-    [Header("Laser")]
-    [SerializeField] private Laser_Mechanic[] laserOpen;
-    [SerializeField] private Laser_Mechanic[] laserClosed;
-    Animator ani;
-    public bool sticks;
-
-    private void Start()
+    public enum TargetType
     {
+        Platform,
+        Door,
+        Laser
+    }
+
+    [System.Serializable]
+    public class TargetEntry
+    {
+        public TargetType type;
+        public GameObject targetObject;
+        public bool activateOnTrigger; // true: activar, false: desactivar
+        [HideInInspector] public IActivable activable;
+        [HideInInspector] public MovingPlatform platform;
+    }
+
+    [Header("Switch Settings")]
+    [SerializeField] private List<TargetEntry> targets = new List<TargetEntry>();
+    [SerializeField] private bool isToggleSwitch = false; // Reemplaza 'sticks'
+    [SerializeField] private ParticleSystem activateEffect;
+    [SerializeField] private AudioSource activateSound;
+    [SerializeField] private AudioSource deactivateSound;
+
+    private Animator ani;
+    private SpriteRenderer sr;
+    private bool isActivated = false;
+
+    protected override void Start()
+    {
+        base.Start();
         ani = GetComponent<Animator>();
-    }
-    private void OnTriggerStay2D()
-    {
-        ani.SetBool("goDown", true);
+        sr = GetComponent<SpriteRenderer>();
 
-        foreach(Door_Mechanic trigger in doorOpen)
+        // Indicador visual para polaridad
+        if (requiresSpecificPolarity && sr != null)
         {
-            trigger.Toggle(true);
+            sr.color = requiredPolarityIsPositive ? Color.red : Color.blue;
         }
 
-        foreach(Door_Mechanic closed in doorClosed)
+        // Inicializar objetivos
+        foreach (var target in targets)
         {
-            closed.ignoreTrigger = true;
-            closed.Toggle(false);
-        }
-        foreach(Laser_Mechanic lasertrigger in laserOpen)
-        {
-            lasertrigger.Toggle(true);
-        }
-        foreach(Laser_Mechanic laserClosed in laserClosed)
-        {
-            laserClosed.ignoreTrigger = true;
-            laserClosed.Toggle(false);
-        }
-    }
-    private void OnTriggerExit2D()
-    {
-        if (sticks)
-            return;
-        ani.SetBool("goDown", false);
+            if (target.targetObject == null)
+            {
+                Debug.LogWarning($"Switch_Mechanic '{gameObject.name}': Un objetivo en la lista 'Targets' no tiene GameObject asignado.");
+                continue;
+            }
 
-        foreach (Door_Mechanic trigger in doorOpen)
-        {
-            trigger.Toggle(false);
-        }
-        foreach (Door_Mechanic closed in doorClosed)
-        {
-            closed.ignoreTrigger = false;
-            closed.Toggle(true);
-        }
-        foreach (Laser_Mechanic lasertrigger in laserOpen)
-        {
-            lasertrigger.Toggle(false);
-        }
-        foreach (Laser_Mechanic laserClosed in laserClosed)
-        {
-            laserClosed.ignoreTrigger = false;
-            laserClosed.Toggle(true);
+            switch (target.type)
+            {
+                case TargetType.Platform:
+                    target.platform = target.targetObject.GetComponent<MovingPlatform>();
+                    if (target.platform == null)
+                    {
+                        Debug.LogWarning($"Switch_Mechanic '{gameObject.name}': El objetivo '{target.targetObject.name}' no tiene un componente MovingPlatform.");
+                    }
+                    break;
+                case TargetType.Door:
+                case TargetType.Laser:
+                    target.activable = target.targetObject.GetComponent<IActivable>();
+                    if (target.activable == null)
+                    {
+                        Debug.LogWarning($"Switch_Mechanic '{gameObject.name}': El objetivo '{target.targetObject.name}' no tiene un componente IActivable.");
+                    }
+                    break;
+            }
         }
     }
-    private void OnDrawGizmos()
+
+    public override void Interact()
     {
-        Gizmos.color = Color.cyan;
-        foreach (Door_Mechanic trigger in doorOpen)
+        if (isActivated && !isToggleSwitch) return;
+
+        var player = FindFirstObjectByType<Player>();
+        if (requiresSpecificPolarity && player != null)
         {
-            Gizmos.DrawLine(transform.position, trigger.transform.position);
+            var batteryController = player.battery.GetComponent<BatteryController>();
+            if (batteryController.isPositivePolarity != requiredPolarityIsPositive)
+            {
+                Debug.Log($"Switch_Mechanic '{gameObject.name}': Polaridad incorrecta. Se requiere {(requiredPolarityIsPositive ? "positiva" : "negativa")}.");
+                if (deactivateSound != null) deactivateSound.Play();
+                return;
+            }
         }
-        Gizmos.color = Color.yellow;
-        foreach (Door_Mechanic closed in doorClosed)
+
+        ToggleSwitch(!isActivated);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (isActivated && isToggleSwitch) return;
+
+        if (requiresSpecificPolarity)
         {
-            Gizmos.DrawLine(transform.position, closed.transform.position);
+            BatteryController batteryController = null;
+            if (other.CompareTag("Player"))
+            {
+                var player = other.GetComponent<Player>();
+                batteryController = player?.battery.GetComponent<BatteryController>();
+            }
+            else if (other.CompareTag("Battery"))
+            {
+                batteryController = other.GetComponent<BatteryController>();
+            }
+
+            if (batteryController != null && batteryController.isPositivePolarity != requiredPolarityIsPositive)
+            {
+                Debug.Log($"Switch_Mechanic '{gameObject.name}': Polaridad incorrecta. Se requiere {(requiredPolarityIsPositive ? "positiva" : "negativa")}.");
+                if (deactivateSound != null) deactivateSound.Play();
+                return;
+            }
+        }
+
+        if (!isActivated)
+        {
+            ToggleSwitch(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (isToggleSwitch) return;
+
+        if (isActivated)
+        {
+            ToggleSwitch(false);
+        }
+    }
+
+    private void ToggleSwitch(bool activate)
+    {
+        isActivated = activate;
+        isActive = activate;
+
+        if (ani != null)
+        {
+            ani.SetBool("goDown", activate);
+        }
+
+        if (activate)
+        {
+            if (activateEffect != null) activateEffect.Play();
+            if (activateSound != null) activateSound.Play();
+        }
+        else
+        {
+            if (deactivateSound != null) deactivateSound.Play();
+        }
+
+        foreach (var target in targets)
+        {
+            bool targetState = activate ? target.activateOnTrigger : !target.activateOnTrigger;
+            if (target.type == TargetType.Platform && target.platform != null)
+            {
+                if (targetState) target.platform.Activate();
+                else target.platform.Deactivate();
+            }
+            else if ((target.type == TargetType.Door || target.type == TargetType.Laser) && target.activable != null)
+            {
+                target.activable.Toggle(targetState);
+                target.activable.SetIgnoreTrigger(!targetState);
+            }
+        }
+
+        Debug.Log($"Switch_Mechanic '{gameObject.name}': {(activate ? "Activado" : "Desactivado")}");
+    }
+
+    private new void OnDrawGizmos()
+    {
+        foreach (var target in targets)
+        {
+            if (target.targetObject == null) continue;
+            Gizmos.color = target.activateOnTrigger ? Color.cyan : Color.yellow;
+            Gizmos.DrawLine(transform.position, target.targetObject.transform.position);
         }
     }
 }
